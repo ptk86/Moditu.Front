2780fe81613e93c14c89bb21ee805def7294a22c
Piotr Kiliński <piotr.kilinski@gmail.com>
2018-06-16 22:00:35 +0200

    Initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <button mat-button>Click me!</button>
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'app';
+}
